ce0c2a64e81e86af5cc0efcd84b3b0642f6fcb0b
medhatiwari <medhavns1@gmail.com>
2021-10-24 18:57:51 +0530

    #17 contract linking with flutter cont..

--- lib/contract_linking.dart
+++ lib/contract_linking.dart
@@ -2,4 +2,20 @@ import 'package:flutter/material.dart';
 
 class ContractLinking extends ChangeNotifier {
 
+  final String _rpcUrl = "http://10.0.2.2:7545";
+  final String _wsUrl = "ws://10.0.2.2:7545/";
+  final String _privateKey = " Private Key :) ";
+
+  Web3Client _client;
+
+  String _abiCode;
+  EthereumAddress _contractAddress;
+
+  ContractFunction _bidderName;
+  ContractFunction _bidAmount;
+  ContractFunction _minAmount;
+  ContractFunction _setBidder;
+  ContractFunction _minBid;
 }
+
+
